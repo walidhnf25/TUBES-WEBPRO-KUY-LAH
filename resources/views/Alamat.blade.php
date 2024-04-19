64e1c36e7ff79f9edb38102477104514517fe176
Walid Hanif Ataullah <walidhnf12@gmail.com>
2024-04-19 18:04:01 +0700

    Penambahan fitur alamat event

--- resources/views/Alamat.blade.php
+++ resources/views/Alamat.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>My Simple Webpage</title>
+    <title>Alamat Event</title>
     <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link href="{{asset('assets/css/Alamat.css')}}" rel="stylesheet">
