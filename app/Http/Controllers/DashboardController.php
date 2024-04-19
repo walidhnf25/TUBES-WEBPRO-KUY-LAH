373dcfcf934890626e256ebed4ee2c77f379cf69
Walid Hanif Ataullah <walidhnf12@gmail.com>
2024-04-19 17:35:36 +0700

    Perubahan sedikit pada Migrations

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Events;
 class DashboardController extends Controller
 {
-    //
     // Menampilkan list pada table halaman Events
     public function index(){
         $event = Events::get();
